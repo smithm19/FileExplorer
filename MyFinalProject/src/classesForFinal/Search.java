8f45b107add6c64f5a329416e1c6949b612f5ff3
Matthew Smith <smithm19@wit.edu>
2014-11-25 13:15:12 -0500

    me0w

--- MyFinalProject/src/classesForFinal/Search.java
+++ MyFinalProject/src/classesForFinal/Search.java
@@ -1,72 +1,56 @@
 package classesForFinal;
 
-
-
-
 import java.nio.file.*;
 import java.io.*;
 import java.nio.file.attribute.*;
 
-
-
 public class Search {
 
-	
-	
-	public static void main(String[] args) 
-			throws IOException{				//Files.walkFileTree doesn't work with the throw
+	public static void main(String[] args) throws IOException // Files.walkFileTree doesn't work without the throw
 	{
-		
-		
-		
-		Path startingDir = Paths.get(args[0]);
-		String pattern = args[2];
-	    Finder finder = new Finder(pattern);
-	    Files.walkFileTree(startingDir, finder);
+		{
+
+			Path startingDir = Paths.get(args[0]);
+			String pattern = args[2];
+			Finder testFind = new Finder(pattern);
+			Files.walkFileTree(startingDir, testFind);
+		}
 	}
-	}
-	
-	public static class Finder extends SimpleFileVisitor<Path>
-	{
-		
-		private static final FileVisitResult CONTINUE = null;			//constant variable from SimpleVisitResult
-	    private final PathMatcher match;
-	    private int numMatches = 0;				//record number of matches
-	    
-	    
-	    Finder(String test)
-	    {
-	    	match = FileSystems.getDefault().getPathMatcher("glob:" + test);
-	    }
-	    
-	    void find(Path file) 
-	    {
-	        Path name = file.getFileName();
-	        if (name != null && match.matches(name)) 
-	        {
-	            numMatches++;
-	            System.out.println(file);
-	        }
-	    }
-	    
-	    void results()
-	    {
-	    	System.out.println(numMatches + " Matches");
-	    }
-	    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) 
-	    {
-	        find(file);
-	        return CONTINUE;
-	    }
-	    
-	    public FileVisitResult preVisitDirectory(Path dir,BasicFileAttributes attrs) 
-	    {
-	        find(dir);
-	        return CONTINUE;
-	    }
-	    
-	    
-	    
+
+	public static class Finder extends SimpleFileVisitor<Path> {
+
+		private static final FileVisitResult CONTINUE = null; // constant
+																// variable from
+																// SimpleVisitResult
+		private final PathMatcher match;
+		private int numMatches = 0; // record number of matches
+
+		Finder(String test) {
+			match = FileSystems.getDefault().getPathMatcher("glob:" + test);
+		}
+
+		void find(Path file) {
+			Path name = file.getFileName();
+			if (name != null && match.matches(name)) {
+				numMatches++;
+				System.out.println(file);
+			}
+		}
+
+		void results() {
+			System.out.println(numMatches + " Matches");
+		}
+
+		public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
+			find(file);
+			return CONTINUE;
+		}
+
+		public FileVisitResult preVisitDirectory(Path dir,
+				BasicFileAttributes attrs) {
+			find(dir);
+			return CONTINUE;
+		}
+
 	}
 }
-
