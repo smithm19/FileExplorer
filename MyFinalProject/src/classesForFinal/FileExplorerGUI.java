9cd2a882c73f4163189f2a86aca056c16111ed0b
Matthew Smith <smithm19@wit.edu>
2014-12-05 08:21:29 -0500

    Updating GUI code

--- MyFinalProject/src/classesForFinal/FileExplorerGUI.java
+++ MyFinalProject/src/classesForFinal/FileExplorerGUI.java
@@ -6,24 +6,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTree;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
 import javax.swing.tree.DefaultMutableTreeNode;
 
+
+@SuppressWarnings("unused")
 public class FileExplorerGUI {
 	
 	private JTree fileTree;				//create JTree
 	private JScrollPane treeScroll;
 	private JFrame frame;
+	private JFrame popFrame;
+	private JList popList;
 	private JTextField itemToSearch;
+	private DefaultListModel listCreationData = new DefaultListModel();
 	
 	public static void main(String[] args)
 	{
 		FileExplorerGUI test = new FileExplorerGUI();
 		test.fileHierarchy("C:\\Users\\smithm19\\Documents");
 		test.createGUI();
+		
 		
 	}
 	
@@ -48,14 +58,27 @@ public class FileExplorerGUI {
 			@Override
 			public void actionPerformed(ActionEvent arg0) 
 			{
-				// TODO Auto-generated method stub
+				
 				if (arg0.getActionCommand().equals("Enter"))
 				{
 					if(itemToSearch.getText() != null && !itemToSearch.getText().equals(""))
 					{
-						if (itemToSearch.getText().indexOf("\\") !=-1)			//searches file for //
+						if (itemToSearch.getText().indexOf("\\") !=-1)			//searches file for \\
 							{
-								recreateTree();	
+								//recreateTree();	
+
+								popFrame = new JFrame("Results");
+								popFrame.setLocationRelativeTo(null);
+								popFrame.setSize(300, 200);
+								popFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+								popFrame.setVisible(true);
+								createArrayList();
+								popList = new JList(listCreationData);
+								popList.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+								popList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+								popList.setVisibleRowCount(-1);								
+								popFrame.setLayout(new BorderLayout());
+								popFrame.add(popList,BorderLayout.CENTER);
 							}
 						else
 						{
@@ -68,14 +91,33 @@ public class FileExplorerGUI {
 	}
 	private void recreateTree() 
 	{
-	// TODO Auto-generated method stub
+//		fileHierarchy(itemToSearch.getText());
+//		frame.setVisible(false);
+//		frame.remove(treeScroll);
+//		treeScroll = new JScrollPane(fileTree);
+//		frame.add(treeScroll,BorderLayout.CENTER);
+//		frame.setVisible(true);
+	}
 	
-		fileHierarchy(itemToSearch.getText());
-		frame.setVisible(false);
-		frame.remove(treeScroll);
-		treeScroll = new JScrollPane(fileTree);
-		frame.add(treeScroll,BorderLayout.CENTER);
-		frame.setVisible(true);
+	private void createArrayList()
+	{
+		listCreationData.addElement("me0w");
+		File root = new File("C:\\Users\\smithm19\\Documents");
+		File[] files = root.listFiles();
+		String match = itemToSearch.getText();
+		for (File file : files)
+		{
+			String fileName = file.toString();
+			listCreationData.addElement("in");
+			if (fileName.toLowerCase().indexOf(match.toLowerCase()) >= 0)
+				listCreationData.addElement(file);
+		}
+		
+		for (int i = 0; i < files.length; i++)
+		{
+			
+		}
+		System.out.println(listCreationData.size());
 	}
 				
 				
--- MyFinalProject/src/classesForFinal/Search.java
+++ MyFinalProject/src/classesForFinal/Search.java
@@ -6,36 +6,55 @@ import java.nio.file.attribute.*;
 
 public class Search {
 
-	public static void main(String[] args) throws IOException // Files.walkFileTree doesn't work without the throw
+	public static void main(String[] args) throws IOException
 	{
-		{
-
-			Path startingDir = Paths.get(args[0]);
-			String pattern = args[2];
+		{			
+			Search t = new Search();
+			t.startSearch("Java*");
+			
+			String pattern = ("*.java");
+			PathMatcher matcher =
+			    FileSystems.getDefault().getPathMatcher("glob:" + pattern);
+			System.out.println(matcher);
+		}
+	}
+	
+	public PathMatcher testingShit(String pat)
+	{
+		PathMatcher matcher =
+			    FileSystems.getDefault().getPathMatcher("glob:" + pat);
+		return matcher;
+	}
+	public void startSearch(String toFind) throws IOException // Files.walkFileTree doesn't work without the throw
+	{
+			Path startingDir = Paths.get("C:\\Users\\smithm19\\My Documents");
+			String pattern = toFind;
 			Finder testFind = new Finder(pattern);
 			Files.walkFileTree(startingDir, testFind);
-		}
 	}
 
 	public static class Finder extends SimpleFileVisitor<Path> {
 
-		private static final FileVisitResult CONTINUE = null; // constant
+		private final FileVisitResult CONTINUE = null; // constant
 																// variable from
 																// SimpleVisitResult
-		private final PathMatcher match;
+		private PathMatcher match;
 		private int numMatches = 0; // record number of matches
 
 		Finder(String test) {
 			match = FileSystems.getDefault().getPathMatcher("glob:" + test);
 		}
 
-		void find(Path file) {
+		Path find(Path file) {
 			Path name = file.getFileName();
 			if (name != null && match.matches(name)) {
 				numMatches++;
 				System.out.println(file);
+				return file;
 			}
+			return null;
 		}
+		
 
 		void results() {
 			System.out.println(numMatches + " Matches");
@@ -53,4 +72,5 @@ public class Search {
 		}
 
 	}
+	
 }
