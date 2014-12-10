e37c38c251229a20ca1ba3b9b0ebab6cbe8582a6
Matthew Smith <smithm19@wit.edu>
2014-12-10 14:34:06 -0500

    Removed class files from future commits.

--- MyFinalProject/src/classesForFinal/Search.java
+++ MyFinalProject/src/classesForFinal/Search.java
@@ -5,35 +5,31 @@ import java.io.File;
 import javax.swing.DefaultListModel;
 
 public class Search {
-	
-	void createArrayList(File base, String itemToSearch,DefaultListModel listCreationData ) throws InterruptedException {
+	void createArrayList(File base, String itemToSearch,
+			DefaultListModel listCreationData) throws InterruptedException {
 
-		
-		
 		File root = null;
 		root = base;
 		File[] files = root.listFiles();
 		String match = itemToSearch.toLowerCase();
-		if (files!= null){
-		for (int i = 0; i < files.length; i++)
-		{
-			String fileName = files[i].getName();
+		if (files != null) {
+			for (int i = 0; i < files.length; i++) {
+				String fileName = files[i].getName();
 
-			// Searches
-			
-			if (fileName.toLowerCase().startsWith(match)) {
-				listCreationData.addElement(files[i]);
-			}
+				// Searches
+				if (fileName.toLowerCase().startsWith(match)) {
+					listCreationData.addElement(files[i]);
+				}
 
-			else if (fileName.toLowerCase().endsWith(match)) {
-				listCreationData.addElement(files[i]);
-			}
+				else if (fileName.toLowerCase().endsWith(match)) {
+					listCreationData.addElement(files[i]);
+				}
 
-			if (files[i].isDirectory()) {
-			 	System.out.println(files[i]);
-				createArrayList(files[i],itemToSearch,listCreationData);
+				if (files[i].isDirectory()) {
+					System.out.println(files[i]);
+					createArrayList(files[i], itemToSearch, listCreationData);
+				}
 			}
-		}
 		}
 	}
 
